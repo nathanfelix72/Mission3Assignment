b22fc44deb6be8bb291876d69f26cb31db0bc791
Nathan Felix <nathan.felix@gmail.com>
2025-01-21 18:00:03 -0700

    Add project files.

---
+++ FoodItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission3Assignment
+{
+    internal class FoodItem
+    {
+        public string name;
+        public string category;
+        public int quantity;
+        public string expirationDate;
+
+        public FoodItem(string name, string category, int quantity, string expirationDate)
+        {
+            this.name = name;
+            this.category = category;
+            this.quantity = quantity;
+            this.expirationDate = expirationDate;
+        }
+
+        public override string ToString()
+        {
+            return $"- {name}:\n    Category: {category}\n    Quantity: {quantity}\n    Expiration Date: {expirationDate}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿using Mission3Assignment;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool game = true;
+        List<FoodItem> foodItems = new List<FoodItem>();
+
+        while (game == true)
+        {
+            Console.WriteLine("What would you like to do? Enter a number");
+            Console.WriteLine("1. Add a food item to the list (or update quantity)");
+            Console.WriteLine("2. Delete a food item from the list.");
+            Console.WriteLine("3. Display the list of food items.");
+            Console.WriteLine("4. Exit the program.");
+
+            string user_choice = Console.ReadLine();
+
+            if (user_choice == "1")
+            { 
+                int additionalQuantity;
+
+                Console.WriteLine();
+                Console.WriteLine("Enter the name of the food item: ");
+                string foodItemName = Console.ReadLine();
+
+                var existingFoodItem = foodItems.FirstOrDefault(item => item.name == foodItemName);
+
+                if (existingFoodItem != null)
+                {
+                    Console.WriteLine($"You already have {foodItemName} in your inventory.");
+                    Console.WriteLine($"Current quantity: {existingFoodItem.quantity}");
+                    Console.WriteLine("Enter the quantity to add or subtract: ");
+                    do
+                    {
+                        if (int.TryParse(Console.ReadLine(), out additionalQuantity))
+                        {
+                            if (existingFoodItem.quantity + additionalQuantity >= 0)
+                            {
+                                existingFoodItem.quantity += additionalQuantity;
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("You are trying to subtract too much.");
+                                Console.WriteLine("Enter the quantity to add or subtract: ");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter a valid number.");
+                        }
+                    } while (true);
+                }
+                else
+                {
+                    int quantity;
+
+                    Console.WriteLine("Enter the category of the food item: ");
+                    string category = Console.ReadLine();
+                    do
+                    {
+                        Console.WriteLine("Enter the quantity of the food item: ");
+                        if (int.TryParse(Console.ReadLine(), out quantity) && quantity >= 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Quantity must be a non-negative number.");
+                        }
+                    } while (true);
+
+                    Console.WriteLine("Enter the expiration date of the food item: ");
+                    string expirationDate = Console.ReadLine();
+
+                    FoodItem newFoodItem = new FoodItem(foodItemName, category, quantity, expirationDate);
+                    
+                    foodItems.Add(newFoodItem);
+                }
+                Console.WriteLine();
+            }
+            else if (user_choice == "2")
+            {
+                Console.WriteLine();
+                Console.WriteLine("What food item would you like to delete?");
+                Console.WriteLine("(" + string.Join(", ", foodItems.Select(f => f.name)) + ")");
+                string foodItemName = Console.ReadLine();
+
+                var itemToRemove = foodItems.FirstOrDefault(item => item.name == foodItemName);
+                if (itemToRemove != null)
+                {
+                    foodItems.Remove(itemToRemove);
+                    Console.WriteLine($"Removed {foodItemName}.");
+                }
+                else
+                {
+                    Console.WriteLine("Food item not found.");
+                }
+                Console.WriteLine();
+            }
+            else if (user_choice == "3")
+            {
+                Console.WriteLine();
+                foreach (FoodItem foodItem in foodItems)
+                {
+                    Console.WriteLine(foodItem);
+                }
+                Console.WriteLine();
+            }
+            else if (user_choice == "4")
+            {
+                game = false;
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number between 1 and 4.");
+                Console.WriteLine();
+            }
+        }
+    }
+}
